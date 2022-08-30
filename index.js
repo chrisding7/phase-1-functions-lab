640cd71645f0e36496d8237746d71e9763159c0f
Chris Ding <chrisding7@berkeley.edu>
2022-08-30 14:24:20 -0700

    finished function 1 distanceFromHqInBlocks

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    //returns #blocks from hq (42) to pickup location (someValue)
+    return Math.abs(42 - someValue);
+}
