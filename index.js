c3d0443e458ef5372103fd3a305abeb855632237
Cindy Qu <cindyqu42@gmail.com>
2022-08-30 14:34:49 -0700

    completed functions 2 and 3

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function distanceFromHqInBlocks(someValue) {
     //returns #blocks from hq (42) to pickup location (someValue)
     return Math.abs(42 - someValue);
 }
+// convert number of blocks to feet
+function distanceFromHqInFeet(someValue) {
+    // 1 block is 264 ft
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
