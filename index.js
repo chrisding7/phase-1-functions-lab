aa3c9c42b0da23bb101f5efb33bbb9dffbaa47fc
Chris Ding <chrisding7@berkeley.edu>
2022-08-30 14:47:01 -0700

    commit tree bb226d72ae073a601c86640c8eed1444ff16dcff

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ function distanceFromHqInFeet(someValue) {
 function distanceTravelledInFeet(start, end) {
     return Math.abs(start - end) * 264;
 }
+
+function calculatesFarePrice(start, end) {
+    //first 400 feet are free
+    const distance = distanceTravelledInFeet(start, end);
+
+    if(distance <= 400) {
+        return 0;
+    }
+    else if(distance > 400 && distance <= 2000) {
+        return 0.02 * (distance - 400);
+    }
+    else if(distance > 2000 && distance < 2500) {
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
+    }
+}
